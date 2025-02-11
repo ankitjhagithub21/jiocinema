ea3cc60c21aedf5df3dfc4c9f8f132df79c8fb88
ankitjhagithub21 <ajha45075@gmail.com>
2025-02-11 17:53:07 +0530

    added movies data

--- server/index.js
+++ server/index.js
@@ -1,13 +1,28 @@
-import express from "express";
-import cors from "cors"
+const express = require('express')
+const cors = require('cors')
+const fs = require('fs')
+const path = require('path')
 const app = express()
 const port = 3000
 
 app.use(express.json())
 app.use(cors())
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
+const movieFilePath = path.join(__dirname,'data.json')
+
+function readMoviesData(){
+  try{
+    const data = fs.readFileSync(movieFilePath,'utf8')
+    return JSON.parse(data)
+  }catch(error){
+    console.error("Error reading json file",error);
+    return [];
+  }
+}
+
+app.get('/movies', (req, res) => {
+  const movies = readMoviesData();
+  res.json(movies)
 })
 
 app.listen(port, () => {
