6b36e303dfd67883f2d2a46223a5c4b6516b0a0b
ankitjhagithub21 <ajha45075@gmail.com>
2025-02-12 16:49:58 +0530

    minor changes

--- client/src/components/spotlight/SpotLight.jsx
+++ client/src/components/spotlight/SpotLight.jsx
@@ -5,8 +5,8 @@ const SpotLight = () => {
     <div className={Styles.spotlight}>
         <h1>In The Spotlight</h1>
         <div className={Styles.container}>
-            <img src="https://v3img.voot.com/resizeMedium,w_768,h_432/v3Storage/assets/16x9-1739163562207.jpg" alt="" />
-            <img src="https://v3img.voot.com/resizeMedium,w_768,h_432/v3Storage/assets/16x9-1739193323639.jpg" alt="" />
+            <img src="https://v3img.voot.com/resizeMedium,w_768,h_432/v3Storage/assets/16x9-1739163562207.jpg" alt="spotlight" />
+            <img src="https://v3img.voot.com/resizeMedium,w_768,h_432/v3Storage/assets/16x9-1739193323639.jpg" alt="spotlight" />
         </div>
     </div>
   )
