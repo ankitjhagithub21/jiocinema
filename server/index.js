f73b630fdcb593756c2b352b0f1439a14d740823
ankitjhagithub21 <ajha45075@gmail.com>
2025-02-12 16:10:32 +0530

    minor changes

--- server/index.js
+++ server/index.js
@@ -1,12 +1,15 @@
+require('dotenv').config()
 const express = require('express')
 const cors = require('cors')
 const fs = require('fs')
 const path = require('path')
 const app = express()
-const port = 3000
+const port = process.env.PORT || 3000
 
 app.use(express.json())
-app.use(cors())
+app.use(cors({
+  origin:process.env.ORIGIN
+}))
 
 const movieFilePath = path.join(__dirname,'data.json')
 
